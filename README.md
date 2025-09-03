cb2e2b85cd74168f2d8f850bdc08a6d21180ba7d
PedroDiagro <pedro.diagolopes@gmail.com>
2025-09-02 22:15:04 -0300

    EX resolvidos

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 1TIAPR
 Para minhas aulas de Python 
 pertence ao aluno Pedro
+
---
+++ ex1.py
@@ -0,0 +1,14 @@
+print("hello Whord")
+
+A = 5
+B = 6
+
+
+print(f"Valor de a: {5}")
+print(f"Valor de B: {6}")
+
+
+nome = "Pedro"
+idade = 20
+altura = 1.80
+sexo = True
